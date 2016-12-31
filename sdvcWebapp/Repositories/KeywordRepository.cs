e3d4748a8c8b747fb48c6a432d31fb7f8d2c07ef
Todd <toddcullum@gmail.com>
2016-12-31 00:22:21 -0800

    Keyword Repository

--- sdvcWebapp/Repositories/KeywordRepository.cs
+++ sdvcWebapp/Repositories/KeywordRepository.cs
@@ -2,11 +2,29 @@ public class KeywordRepository : IKeywordRepository
 {
     private IDbConnection _db;
 
-    public List<User> GetAll()
+    public List<Keyword> GetAll()
     {
         using(_db = new MySqlConnection(mysqlConfig.Value.ConnectionString))
         {
             return _db.Query<Keyword>("SELECT * FROM \"Keywords\";");
+        }
+    }
+
+    public Keyword FindById(int id)
+    {
+        using(_db = new MySqlConnection(mysqlConfig.Value.ConnectionString))
+        {
+        return _db.Query<Keyword>("SELECT * FROM \"Keywords\"
+                                    WHERE id = @id;");
+        }
+    }
+
+    public Keyword Add(Keyword keyword)
+    {
+        using(_db = new MySqlConnection(mysqlConfig.Value.ConnectionString))
+        {
+            //TODO: Complete
+        return _db.Query<Keyword>("INSERT INTO \"Keywords\" VALUES ");
         }
     }
 }
